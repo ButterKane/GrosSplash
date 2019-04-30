d30c8937e28442122ea3d7dd8a6e6e707613e8ca
ButterKane <alexandre.fourquet@gmail.com>
2019-04-30 15:49:55 +0200

    Dummy enemy created, water pushes it back

---
+++ Assets/Scripts/WaterPush.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPush : MonoBehaviour
+{
+    public ParticleSystem particles;
+    public float pushForce;
+
+    private List<ParticleCollisionEvent> particlesCollisionEvents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particlesCollisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnParticleCollision(GameObject other)
+    {
+        print("Une particle touche");
+        int colCount = particles.GetSafeCollisionEventSize();
+
+        if (colCount > particlesCollisionEvents.Count)
+            particlesCollisionEvents = new List<ParticleCollisionEvent>();
+
+        int eventCount = particles.GetCollisionEvents(other, particlesCollisionEvents);
+
+        for (int i = 0; i < eventCount; i++)
+        {
+            //Make the push back vector
+            Vector3 impactPos = particlesCollisionEvents[i].intersection;
+            float impactDirectionX = other.transform.position.x - impactPos.x;
+            float impactDirectionZ = other.transform.position.z - impactPos.z;
+            Vector3 impactDirection = new Vector3(impactDirectionX, 0f, impactDirectionZ).normalized;
+
+            if (other.GetComponent<Rigidbody>() && other.tag == "Movable")
+            {
+                other.GetComponent<Rigidbody>().AddForce(impactDirection * pushForce, ForceMode.Impulse);
+            }
+        }
+    }
+}

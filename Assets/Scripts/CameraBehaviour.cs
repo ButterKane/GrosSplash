ad4f8b855d930d88d362b6106f18c5dd284d35a8
ButterKane <alexandre.fourquet@gmail.com>
2019-05-02 16:06:02 +0200

    New Camera Behaviour

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CameraBehaviour : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject[] players;
 
+    private void Awake()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +19,17 @@ public class CameraBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = player.transform.position + Vector3.up * 25;
-        transform.LookAt(player.transform.position);
+        Vector3[] playerPositions = new Vector3[players.Length];
+        for (int i = 0; i < players.Length; i++)
+        {
+            playerPositions[i] = players[i].transform.position;
+        }
+
+        Vector3 middlePos = ((playerPositions[1] - playerPositions[0])/ 2) + playerPositions[0];
+
+        float howFarApart = (playerPositions[1] - playerPositions[0]).magnitude;
+
+        transform.position = middlePos + Vector3.up * howFarApart;
+        transform.LookAt(middlePos);
     }
 }

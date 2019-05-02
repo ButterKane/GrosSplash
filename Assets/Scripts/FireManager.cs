574f7fc657634166dc74043572b41890528e28a7
Jules Duvette <julesduvette@gmail.com>
2019-05-02 19:54:12 +0200

    Added flame propagation

---
+++ Assets/Scripts/FireManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireManager : MonoBehaviour
+{
+    [HideInInspector] GridManager gridManager;
+
+    private void Awake()
+    {
+        gridManager = FindObjectOfType<GridManager>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            IgniteTile(new Vector2Int(0, 0), 100);
+        }
+    }
+
+    public void IgniteTile(Vector2Int coordinates, float fireStrength)
+    {
+        if (fireStrength > gridManager.tileGrid[coordinates.x, coordinates.y].GetTileData().fireStrengthToBeSetOnFire)
+        {
+            gridManager.tileGrid[coordinates.x, coordinates.y].Ignite();
+        }
+    }
+}
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,33 +2,60 @@
 
 public class Tile : MonoBehaviour
 {
-    private TileData tileData;
+    public TileData tileData;
     public int rotationAmount; //Between 1 and 4, Y axis
     public Vector2Int coordinates;
-    float fireValue;
+    public GameObject actualFire;
+    public float fireValue;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Water")
+        {
+            fireValue -= 1;
+        }
+    }
 
     private void Awake()
     {
         fireValue = 0;
+        InvokeRepeating("UpdateFire", 0f, 1f);
     }
 
-    private void Update()
+    private void UpdateFire()
     {
         if (fireValue > 0)
         {
             fireValue += tileData.fireIncrementationPerSecond;
             TrySpreadFire();
-        } else
+            if (actualFire != null)
+            {
+                float fireScale = fireValue / 100;
+                foreach (Transform t in actualFire.transform)
+                {
+                    t.localScale = new Vector3(fireScale, fireScale, fireScale);
+                }
+            }
+        }
+        else
         {
             ExtinguishFire();
+        }
+        if (tileData != null)
+        {
+            fireValue = Mathf.Clamp(fireValue, tileData.minFireValue, tileData.maxFireValue);
         }
     }
 
     //Generates the fire on this tile
     public void Ignite()
     {
+        if (fireValue > 0) { return; }
+        if (!GetTileData().inflammable) { return; }
         fireValue++;
         //Generates firevisuals
+        actualFire = Instantiate(GameManager.i.library.fireFX, this.transform, true);
+        actualFire.transform.localPosition = Vector3.zero;
     }
 
     //Extinguish the fire on the actual tile
@@ -36,6 +63,12 @@ public class Tile : MonoBehaviour
     {
         fireValue = 0;
         //Removes the firevisuals
+        if (actualFire != null)
+        {
+            GameObject extinguishFX = Instantiate(GameManager.i.library.extinguishFX, this.transform, true);
+            extinguishFX.transform.localPosition = Vector3.zero;
+            Destroy(actualFire.gameObject);
+        }
     }
 
     //Try to spread the fire to the near tiles
@@ -43,34 +76,57 @@ public class Tile : MonoBehaviour
     {
         //Try to ignite tile north
         Vector2Int coordinatesToCheck = coordinates + new Vector2Int(0, 1);
-        float fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
-        GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        if (GameManager.i.gridManager.CheckIfCoordinatesAreInGrid(coordinatesToCheck))
+        {
+            float fireIsolation = 0;
+            if (GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y] != null)
+            {
+                fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
+            }
+            GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        }
 
         //Try to ignite tile south
         coordinatesToCheck = coordinates + new Vector2Int(0, -1);
-        fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
-        GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        if (GameManager.i.gridManager.CheckIfCoordinatesAreInGrid(coordinatesToCheck))
+        {
+            float fireIsolation = 0;
+            if (GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y] != null)
+            {
+                fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
+            }
+            GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        }
 
         //Try to ignite tile east
         coordinatesToCheck = coordinates + new Vector2Int(1, 0);
-        fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
-        GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        if (GameManager.i.gridManager.CheckIfCoordinatesAreInGrid(coordinatesToCheck))
+        {
+            float fireIsolation = 0;
+            if (GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y] != null)
+            {
+                fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
+            }
+            GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        }
 
         //Try to ignite tile west
         coordinatesToCheck = coordinates + new Vector2Int(-1, 0);
-        fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
-        GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        if (GameManager.i.gridManager.CheckIfCoordinatesAreInGrid(coordinatesToCheck))
+        {
+            float fireIsolation = 0;
+            if (GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y] != null)
+            {
+                fireIsolation = GameManager.i.gridManager.wallGrid[coordinatesToCheck.x, coordinatesToCheck.y].GetWallData().fireIsolation;
+            }
+            GameManager.i.fireManager.IgniteTile(coordinatesToCheck, fireValue - fireIsolation);
+        }
     }
 
     public void ChangeTileData(TileData newTileData)
     {
         tileData = newTileData;
         UpdateTile();
-    }
-
-    public void UpdateFireValue(float incrementationValue)
-    {
-
     }
 
     public TileData GetTileData()

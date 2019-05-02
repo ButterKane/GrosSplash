d7f51886875752d6dbab2054ecf51bedd3c6cd02
BoxCollider <q.roze@rubika-edu.com>
2019-05-02 15:37:18 +0200

    change graphics metas + update menu

---
+++ Assets/Scripts/MenuSlotPlayers.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSlotPlayers : MonoBehaviour
+{
+    public PlayerMenu[] players;
+    public Renderer myRend;
+    int playersOnSlot;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<PlayerMenu>() != null)
+        {
+            playersOnSlot++;
+            ActualizeColor();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerMenu>() != null)
+        {
+            playersOnSlot--;
+            ActualizeColor();
+        }
+    }
+
+    void ActualizeColor()
+    {
+        switch (playersOnSlot)
+        {
+            case 0:
+                myRend.material.color = Color.red;
+                break;
+            case 1:
+                myRend.material.color = Color.yellow;
+                break;
+            case 2:
+                myRend.material.color = Color.green;
+                break;
+        }
+    }
+}

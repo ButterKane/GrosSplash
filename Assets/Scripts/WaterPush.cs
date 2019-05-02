d295d22fb2486bbdbf103dc89670cb47c7939f09
ButterKane <alexandre.fourquet@gmail.com>
2019-05-02 19:44:12 +0200

    Base of Trigger colliding with particles

--- Assets/Scripts/WaterPush.cs
+++ Assets/Scripts/WaterPush.cs
@@ -7,21 +7,32 @@ public class WaterPush : MonoBehaviour
     public ParticleSystem particles;
     public ParticleSystem impactParticles;
     public ParticleSystem impactParticles2;
+    public Transform[] triggerColliders;    //Needs to be initialized with all the trigger of level
 
     public float pushForce;
 
     private List<ParticleCollisionEvent> particlesCollisionEvents;
 
+    // these lists are used to contain the particles which match
+    // the trigger conditions each frame.
+    private List<ParticleSystem.Particle> enterTriggerParticles = new List<ParticleSystem.Particle>();
+
     // Start is called before the first frame update
     void Start()
     {
         particlesCollisionEvents = new List<ParticleCollisionEvent>();
+
+        for (int i = 0; i < triggerColliders.Length; i++)
+        {
+            particles.trigger.SetCollider(i, triggerColliders[i]); //Adds the referenced trigger to the list of trigger to check
+        }
     }
+
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
     public void OnParticleCollision(GameObject other)
@@ -54,6 +65,24 @@ public class WaterPush : MonoBehaviour
                 Instantiate(impactParticles, particlesCollisionEvents[i].intersection, Quaternion.identity);
                 Instantiate(impactParticles2, particlesCollisionEvents[i].intersection, Quaternion.identity);
             }
+
+            if (other.tag == "Debug")
+            {
+                print("trigger enter");
+            }
         }
     }
+
+    public void OnParticleTrigger()
+    {
+        // get the particles which matched the trigger conditions this frame
+        int numEnter = particles.GetTriggerParticles(ParticleSystemTriggerEventType.Enter, enterTriggerParticles);
+
+        // iterate through the particles which entered the trigger
+        for (int i = 0; i < numEnter; i++)
+        {
+            // MAKE HERE WHAT YOU WANT WHEN ENTERING TRIGGER
+        }
+    }
+
 }

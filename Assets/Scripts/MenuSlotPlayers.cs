1a56e8ed59fc574033db2e0a7dcabea06152574e
BoxCollider <q.roze@rubika-edu.com>
2019-05-02 16:28:59 +0200

    update menu

--- Assets/Scripts/MenuSlotPlayers.cs
+++ Assets/Scripts/MenuSlotPlayers.cs
@@ -6,7 +6,14 @@ public class MenuSlotPlayers : MonoBehaviour
 {
     public PlayerMenu[] players;
     public Renderer myRend;
+    public Renderer myQuadRend;
+    public float timeToCharge;
+    public float timeToDecharge;
     int playersOnSlot;
+    bool chargingSlot;
+    [HideInInspector]
+    public float lerpValue;
+
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,12 +23,43 @@ public class MenuSlotPlayers : MonoBehaviour
             ActualizeColor();
         }
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.GetComponent<PlayerMenu>() != null)
         {
             playersOnSlot--;
             ActualizeColor();
+        }
+    }
+
+    private void Update()
+    {
+        ChargingUpdate();
+    }
+
+    void ChargingUpdate()
+    {
+        if (chargingSlot && lerpValue < 1)
+        {
+            lerpValue = Mathf.Clamp01(lerpValue + (Time.deltaTime / timeToCharge));
+            myQuadRend.material.SetFloat("_LerpValue", lerpValue);
+            if (lerpValue >= 1)
+            {
+                switch (gameObject.name) {
+                    case "Level1":
+                        break;
+                    case "Level2":
+                        break;
+                    case "Credits":
+                        break;
+                }
+            }
+        }
+        else if (!chargingSlot && lerpValue > 0)
+        {
+            lerpValue = Mathf.Clamp01(lerpValue - (Time.deltaTime / timeToDecharge));
+            myQuadRend.material.SetFloat("_LerpValue", lerpValue);
         }
     }
 
@@ -34,10 +72,17 @@ public class MenuSlotPlayers : MonoBehaviour
                 break;
             case 1:
                 myRend.material.color = Color.yellow;
+                chargingSlot = false;
                 break;
             case 2:
                 myRend.material.color = Color.green;
+                chargingSlot = true;
                 break;
         }
+    }
+
+    void ChargeLevel()
+    {
+
     }
 }
